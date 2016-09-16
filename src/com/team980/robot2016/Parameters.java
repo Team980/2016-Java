8311b27b5a9c606c557bbabe995b7e64e6f24e54
Luke Myers <19lmyers@gmail.com>
2016-09-15 19:13:32 -0700

    2016-offseason code, part 1
    No arm system or custom drive yet but behaves like the old system

--- src/com/team980/robot2016/Parameters.java
+++ src/com/team980/robot2016/Parameters.java
@@ -5,13 +5,12 @@ public class Parameters {
 	// --- DRIVE SYSTEM ---
 	public static int leftDriveMotorChannel = 1;
 	public static int rightDriveMotorChannel = 0;
-	
-	public static double leftMotorMultiplier = 1.0; //TODO determine these values
-	public static double rightMotorMultiplier = 1.0; //TODO based on encoder tuning
+	public static double maxDriveOutput = 0.6;
 	
 	// --- AUTONOMOUS ---
-	public static double autoDistance = 15.0; //feet
-	public static double autoSpeed = 1.0;
+	public static double autoDistance = 8.0; //feet - TODO set to 22.0 for comp
+	public static double leftAutoSpeed = -0.78;
+	public static double rightAutoSpeed = -0.8;
 	
 	// --- ENCODERS --- 
 	public static int leftDriveEncA = 3;
@@ -24,6 +23,25 @@ public class Parameters {
 	
 	public static int driveEncoderCounts = 1024;
 	
+	// --- ARM SYSTEM ---
+	public static int rollerMotorChannel = 1;
+	public static int armMotorChannel = 2;
+	
+	public static double rollerInSpeed = 1.0;
+	public static double rollerOutSpeed = -1.0;
+	
+	// --- ARM SYSTEM PID ---
+	//TODO
+	
 	// --- JOYSTICKS / DRIVER INPUTS ---
 	public static int driveJsChannel = 0;
+	public static int driveWheelChannel = 1;
+	
+	public static int controlJsChannel = 2;
+	
+	public static int controlJsRollerInButton = 3;
+	public static int controlJsRollerOutButton = 2;
+		
+	public static int controlJsArmDownButton = 7;
+	public static int controlJsArmUpButton = 6;
 }
--- src/com/team980/robot2016/Robot.java
+++ src/com/team980/robot2016/Robot.java
@@ -1,6 +1,8 @@
 
 package com.team980.robot2016;
 
+import edu.wpi.first.wpilibj.CANSpeedController;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -13,14 +15,26 @@ public class Robot extends IterativeRobot {
 	private RobotDrive robotDrive;
 	
 	private Joystick driveStick;
+	private Joystick driveWheel;
 	
 	private Encoder leftDriveEnc;
 	private Encoder rightDriveEnc;
 	
+	private Joystick controlStick;
+	
+	private CANTalon armMotor;
+	private CANTalon rollerMotor;
+	
+	
 	public Robot() {
 		robotDrive = new RobotDrive(Parameters.leftDriveMotorChannel, Parameters.rightDriveMotorChannel);
+		robotDrive.setExpiration(0.1); //TODO what does this do
+		robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true); //TODO not parameterized
+		robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+		robotDrive.setMaxOutput(Parameters.maxDriveOutput);
 		
 		driveStick = new Joystick(Parameters.driveJsChannel);
+		driveWheel = new Joystick(Parameters.driveWheelChannel);
 		
 		leftDriveEnc = new Encoder(Parameters.leftDriveEncA, Parameters.leftDriveEncB);
 		leftDriveEnc.setDistancePerPulse(2*Constants.pi*(Constants.wheelRadius/Constants.inchesInFeet)/Parameters.driveEncoderCounts); 
@@ -29,16 +43,27 @@ public class Robot extends IterativeRobot {
 		rightDriveEnc = new Encoder(Parameters.rightDriveEncA, Parameters.rightDriveEncB);
 		rightDriveEnc.setDistancePerPulse(2*Constants.pi*(Constants.wheelRadius/Constants.inchesInFeet)/Parameters.driveEncoderCounts); 
 		rightDriveEnc.setReverseDirection(Parameters.rightDriveEncInv);
+		
+		controlStick = new Joystick(Parameters.controlJsChannel);
+		
+		armMotor = new CANTalon(Parameters.rollerMotorChannel);
+		armMotor.setControlMode(CANTalon.TalonControlMode.Position.getValue());
+		armMotor.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Absolute);
+		//TODO finish PID on arm encoder
+		
+		rollerMotor = new CANTalon(Parameters.rollerMotorChannel);
 	}
 		
     public void robotInit() {
-    	CameraServer.getInstance().setQuality(50);
+    	CameraServer.getInstance().setQuality(25);
     	CameraServer.getInstance().startAutomaticCapture("cam0");
     }
     
     public void autonomousInit() {
     	leftDriveEnc.reset();
     	rightDriveEnc.reset();
+    	
+    	//TODO init arm & roller motors
     }
 
     public void autonomousPeriodic() {
@@ -50,20 +75,29 @@ public class Robot extends IterativeRobot {
     			currentDistRight > Parameters.autoDistance) {
     		robotDrive.setLeftRightMotorOutputs(0, 0); //stops the robot
     	} else {
-    		robotDrive.setLeftRightMotorOutputs(Parameters.leftMotorMultiplier * Parameters.autoSpeed, 
-    				Parameters.rightMotorMultiplier * Parameters.autoSpeed);
+    		robotDrive.setLeftRightMotorOutputs(Parameters.leftAutoSpeed, Parameters.rightAutoSpeed);
     	}
     	
     	SmartDashboard.putNumber("Current Distance - Left", currentDistLeft);
     	SmartDashboard.putNumber("Current Distance - Right", currentDistRight);
     }
+    
+    public void teleopInit() {
+    	//TODO init arm motor
+    }
 
     public void teleopPeriodic() {
-        robotDrive.arcadeDrive(driveStick, Joystick.AxisType.kY.value, driveStick, Joystick.AxisType.kZ.value);
+        robotDrive.arcadeDrive(driveStick, Joystick.AxisType.kY.value, driveWheel, Joystick.AxisType.kX.value, false);
+        //TODO implement custom drive system
+        
+        //TODO implement arm system
+        
+        //TODO print to SmartDashboard
     }
     
-    public void testPeriodic() {
-    
+    public void disabledInit() {
+    	armMotor.disableControl();
+    	rollerMotor.disableControl();
     }
     
 }
